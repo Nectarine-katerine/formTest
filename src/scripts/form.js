4301f559c967b4eb32f01525abea326c1ebf68ee
Ekaterina <extra33333@mail.ru>
2019-05-24 15:20:37 +0300

    fixed js bugs

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -9,11 +9,11 @@ form.addEventListener('submit', e => {
   const password = form.elements.password;
   const email = form.elements.email;
   const agreement = form.elements.agreement;
-  
+
   if(password.value === '') {
     addFormMessage(password, 'Поле не может быть пустым')
   } else if (password.value.length<4) {
-    addFormMessage(password, 'Пароль должен быть не менее 4 символов');
+    addFormMessage(password, 'Используйте не менее 4 символов');
   }
 
   if(email.value === '') {
@@ -31,7 +31,6 @@ form.addEventListener('submit', e => {
   if(!agreement.checked) {
     addFormMessage(agreement, 'Требуется подтверждение')
   }
-
 });
 
 // при фокусе на инпут убираем класс ошибки
@@ -39,35 +38,43 @@ form.addEventListener('submit', e => {
   input.addEventListener('focus', e => {
     let elem = e.currentTarget.parentElement;
 
-    let messElem = elem.querySelector('.form__error');
-    if(messElem) {
-      elem.removeChild(messElem);
+    const nowError = elem.querySelector('.form__error_active');
+
+    if(nowError) {
+     nowError.classList.remove('form__error_active');
     }
 
     elem.classList.remove('form__row_error');
   });
-  
+
+  // если checkbox в состоянии checked убираем класс ошибки
   agreement.addEventListener('change', e => {
     let elem = e.currentTarget.parentElement;
 
-    let messElem = elem.querySelector('.form__error');
-    if(messElem) {
-      elem.removeChild(messElem);
-    }
+    const nowError = elem.querySelector('.form__error_active');
 
+    if(nowError) {
+     nowError.classList.remove('form__error_active');
+    }
     elem.classList.remove('form__row_error');
   });
 
+  // при фокусе на инпут убираем класс ошибки о существовании email
+  email.addEventListener('focus', e => {
+
+    if(emailError) {
+      emailError.style.display='none';
+    }
+  });
 });
 
-// добавляем (создаем) блок-сообщение об ошибке
+// добавляем блок-сообщение об ошибке
 function addFormMessage(elem, message) {
   let parrent = elem.parentElement;
+  const curError = parrent.querySelector('.form__error');
 
-  let messageContainer = document.createElement('div');
-  messageContainer.classList.add('form__error');
-  messageContainer.innerHTML = message;
-  parrent.appendChild(messageContainer);
+  curError.classList.add('form__error_active');
+  curError.innerHTML = message;
 
   parrent.classList.add('form__row_error');
 }
